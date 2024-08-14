d6aa4087db140db030784aba85666886e9cf86a0
Shamit Soni <shamit.soni4@gmail.com>
2024-08-14 19:01:43 -0400

    Add recycle file option
    Add the option to clean files without permanent deletion by sending
    files to the recycle bin.

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 import os
-
 import argparse
+from send2trash import send2trash
+
+# Example: 'python main.py C:\Users\{User}\Desktop\ .txt delete' ----> Will permanently delete all text files on the Desktop
 
 
-def clean_files(directory, extension):
+# Will recycle files
+def recycle_files(directory, extension):
+    print('(INFO) Files will be moved to the recycle bin.')
+    confirm = input('Enter (y) to continue or any key to exit: ')
+
+    if confirm.strip().lower() == 'y':
+        for root, dirs, files in os.walk(directory):
+            for file in files:
+                if file.endswith(extension):
+                    file_path = os.path.join(root, file)
+                    print(f"Moving file to recycle bin: {file_path}")
+                    send2trash(file_path)
+    return
+
+
+# Will permanently delete files
+def delete_files(directory, extension):
     print('(WARNING) Files will NOT be recycled and will be PERMANENTLY deleted! Proceed with caution.')
     confirm = input('Enter (y) to continue or any key to exit: ')
 
@@ -18,13 +36,18 @@ def clean_files(directory, extension):
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Clean files with a specific extension from a directory.")
+    # Handle command line arguments
+    parser = argparse.ArgumentParser(description="Handle files with a specific extension from a directory.")
     parser.add_argument("directory", type=str, help="Directory to clean")
-    parser.add_argument("extension", type=str, help="File extension to remove (e.g., .txt)")
-
+    parser.add_argument("extension", type=str, help="File extension to handle (e.g., .txt)")
+    parser.add_argument("action", type=str, choices=['delete', 'recycle'], help="Action to perform on the files (delete or recycle)")
     args = parser.parse_args()
 
-    clean_files(args.directory, args.extension)
+    # Map each action argument to the respective function
+    map_actions = {'delete': delete_files, 'recycle': recycle_files}
+
+    # Call the function based on the input
+    map_actions[args.action](args.directory, args.extension)
 
 
 if __name__ == "__main__":
